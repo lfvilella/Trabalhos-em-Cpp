993667d734f180d79e82def3f27da87ec101490b
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-05-15 10:39:20 -0300

    Repair Hospedagem

--- hospedagem_na_praia.cpp
+++ hospedagem_na_praia.cpp
@@ -2,6 +2,7 @@
 # include <iostream>
 # include <string>
 # include <string.h>
+# include <ctype.h>
 
 using namespace std;
 
@@ -11,8 +12,11 @@ int main(){
     string tipo_do_apartamento;
     int dias_hospedado;
 
-    // Problema em ler variavel com espacos
-    scanf(" %[^\n]s %d", &tipo_do_apartamento, &dias_hospedado);
+    // Le com espaco
+    getline(cin, tipo_do_apartamento);
+    scanf("%d", &dias_hospedado);
+
+    tipo_do_apartamento = tolower(tipo_do_apartamento);
 
     printf("%.2f \n", Calcula_a_Hospedagem(tipo_do_apartamento, dias_hospedado));
 
@@ -23,9 +27,9 @@ float Calcula_a_Hospedagem(string apartamento, int dias){
     float desconto, total, diaria;
     if (apartamento == "individual"){
         if(dias >= 3){
-                diaria = dias*125;
-                desconto = diaria*0.15;
-                total = diaria-desconto;
+            diaria = dias*125;
+            desconto = diaria*0.15;
+            total = diaria-desconto;
         }
         else{
             total = dias*125;
@@ -33,22 +37,22 @@ float Calcula_a_Hospedagem(string apartamento, int dias){
     }
     else if (apartamento == "suite dupla"){
         if(dias >= 3){
-                diaria = dias*140;
-                desconto = diaria*0.15;
-                total = diaria-desconto;
+            diaria = dias*140;
+            desconto = diaria*0.15;
+            total = diaria-desconto;
         }
         else{
-            total = dias*125;
+            total = dias*140;
         }
     }
     else if (apartamento == "suite tripla"){
         if(dias >= 3){
-                diaria = dias*180;
-                desconto = diaria*0.15;
-                total = diaria-desconto;
+            diaria = dias*180;
+            desconto = diaria*0.15;
+            total = diaria-desconto;
         }
         else{
-            total = dias*125;
+            total = dias*180;
         }
     }
     else{
@@ -57,3 +61,25 @@ float Calcula_a_Hospedagem(string apartamento, int dias){
 
     return total;
 }
+
+// #include <iostream>
+// #include <string>
+// #include <ctype.h>
+// using namespace std;
+
+// int main(){
+//     int i = 0;
+//     string nome;
+//     char c;
+//     printf("digite a seu nome: ");
+//     getline(cin, nome);
+
+//     while (nome[i]){
+//         putchar(tolower(nome[i]));
+//         i++;
+//     }
+
+//     printf("\n");
+
+//     return 0;
+// }
