5f8536cc01a197e0e718c58d81a90b79c67f75c7
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-09-09 13:10:27 -0300

    ordenando nomes

---
+++ Semestre_2-2019/struct/ordenando_por_ordem_alfabetica.cpp
@@ -0,0 +1,35 @@
+# include <stdio.h>
+# include <string.h>
+
+char nome[10][80];
+
+void Ordena(){
+    int i, j, r;
+    char auxiliar[80];
+    for (i = 0; i < 10; i++){
+        for (j = i+1; j < 10; j++){
+            r = strcmp(nome[i], nome[j]); // Retorna 0, 1 ou -1
+            if (r > 0){
+                strcpy(auxiliar, nome[i]);
+                strcpy(nome[i], nome[j]);
+                strcpy(nome[j], auxiliar);
+            }
+        }
+    }
+
+
+}
+int main(){
+    printf("Digite 10 nomes:\n");
+    for (int i = 0; i < 10; i++){
+        gets(nome[i]);
+    }
+
+    Ordena();
+
+    printf("Nomes Ordenados: \n");
+    for (int i = 0; i < 10; i++){
+        puts(nome[i]); // Imprime e pula linha
+    }
+    return 0;
+}
