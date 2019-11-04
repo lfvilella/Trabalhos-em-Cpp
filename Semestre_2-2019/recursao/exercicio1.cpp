15f0237ef4ebee97c963c94a6939d3a3268eba5a
Felipe Vilella <vilella.luisfelipe@gmail.com>
2019-11-04 08:46:40 -0300

    Include Mmc

--- Semestre_2-2019/recursao/exercicio1.cpp
+++ Semestre_2-2019/recursao/exercicio1.cpp
@@ -1,11 +1,13 @@
 # include <stdio.h>
 
 int Mdc(int a, int b);
+int Mmc(int x, int y);
 
 int main(){
     int a,b;
     scanf("%d %d", &a,&b);
     printf("%d\n", Mdc(a,b));
+    printf("%d\n", Mmc(a,b));
     return 0;
 }
 
@@ -15,4 +17,15 @@ int Mdc(int x, int y){
     }
 
 	return Mdc(y, x % y);
+}
+
+int Mmc(int x, int y){
+    int div = 0;
+    if (y == 0){
+        return x;
+    }
+    else{
+        div = (x*y)/(Mdc(x,y));
+    }
+    return div;
 }
